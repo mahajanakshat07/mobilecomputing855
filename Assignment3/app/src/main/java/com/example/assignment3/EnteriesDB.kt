684947b95279fc45f034d8bbccd64436dd747b30
Akshat Mahajan <akshatmahajan1010@gmail.com>
2019-12-04 14:03:57 -0600

    Final Project Added

--- Assignment3/app/src/main/java/com/example/assignment3/EnteriesDB.kt
+++
@@ -1,9 +0,0 @@
-package com.example.assignment3
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [(Enteries::class)],version = 1)
-abstract class EnteriesDB:RoomDatabase(){
-    abstract fun safe() : EnteriesDao
-}
